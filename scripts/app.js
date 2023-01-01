ca1eb5e1ef21a331c93231baa784ae4121c95f41
Pawel Michalski <michalskipawel1998@gmail.com>
2023-01-01 14:53:10 -0600

    initial webpage with 16x16 grid of square divs

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+const gridContainer = document.querySelector("#grid-container");
+
+function generateGrid(numCells) {
+  for (let i = 0; i < numCells; i++) {
+    let row = document.createElement("div");
+    row.className = "grid-row";
+    for (let j = 0; j < numCells; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("grid-cell", "tight-layout", "background-color-whitesmoke");
+      cell.innerText = i*numCells + j;
+      row.appendChild(cell);
+    }
+    gridContainer.appendChild(row);
+  }
+}
+
+generateGrid(16);
